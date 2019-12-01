810012a4aba35e2303ea23de7b02b0c38d63c527
Michael Flynn <flynn.michael@protonmail.com>
2019-12-01 12:47:47 -0500

    style: Reformat to follow PEP 8

---
+++ README.md
@@ -0,0 +1,21 @@
+# What is zeta_combiner?
+Zeta_combiner is a Python script that combines replicate electrophoretic mobility / ELS / PALS / zeta potential distributions performed by a Brookhaven NanoBrook Omni. This was created because the Brookhaven NanoBrook Omni instrument/data processing software I had access to could not export all replicate scans of a sample into one file except as a difficult-to-digitize PDF report, but could export each replicate scan as a separate .csv file.
+# Requirements
+
+- Python 3.8+
+- Matplotlib 3.1.2+
+- NumPy 1.17.4+
+- OpenPyxl 3.0.2+
+- Pandas 0.25.3+
+- XlsxWriter 1.2.6+
+# How to use zeta_combiner
+Zeta_combiner is run by copying zeta_combiner into a folder containing one or more sets of replicate data files that were manually exported from the Brookhaven software in .csv format with identical names except for their endings (Sample-1.csv, Sample-2.csv, etc.), then running the script from the terminal with the following syntax
+
+`python C:/example/path/to/data/zeta_combiner.py`
+
+For each group of samples, zeta_combiner will export
+
+- an .xlsx file for each sample containing the raw and combined zeta potential distributions of that sample's replicate data files
+- an .svg vectorized image of a plot of the combined zeta potential distribution.
+# How zeta_combiner works
+Zeta_combiner first identifies all of the unique zeta potential (x-axis) values that appear in at least one of the replicate data files. The Brookhaven NanoBrook Omni does not use a fixed bin size on the x-axis, so every replicate file has a different set of x-axis values. Zeta_combiner then calculates the mean, sample standard deviation, and number of power (y-axis) values that were collected at each unique x-axis value. Due to the variable bin size of the x-axes from each replicate data file and the fact that each replicate data file is normalized to a mode of 1.0 power, the mean zeta potential distribution can have large local discontinuities that can be reduced with a simple 1-D box blur / moving average. This smoothing can be adjusted in the source code through `nSmooths` (default 2) and `w` (default 1% of x-axis).
--- zeta_combiner.py
+++ zeta_combiner.py
@@ -1,96 +1,134 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Dec  1 14:15:42 2019
+'''ELSCompiler combines multiple zeta potential scans into one Excel workbook
 
-@author: Michael Flynn - Sonnest Inc.
-"""
+ELSCompiler combines multiple electrophoretic mobility / zeta potential scans
+performed by a Malvern NanoBrook Omni and exported as separate csv files,
+because the Malvern database software cannot export all repeat scans of the
+same sample into one file except as a PDF report.
+'''
+__author__ = "Michael Flynn"
+__date__ = "20191201"
 
-import pandas as pd
-import numpy as np
+from copy import deepcopy
 from os import path, listdir
 from matplotlib import pyplot as plt
-from copy import deepcopy
+import numpy as np
+import pandas as pd
 
-### GET DIRECTORY CONTAINING THIS SCRIPT ###
+# GET DIRECTORY CONTAINING THIS SCRIPT
 try:
     currentFile = path.realpath(__file__).replace('\\','/')
 except:
-    currentFile = input('Python needs your help determining the location of this script.\n' + \
-                        'Please copy the full filepath of this Python script from above,\nthen paste it here using right-click.' + \
-                        'Then press Enter to begin.\n')
+    currentFile = input(
+        ('Python needs your help determining the location of this script.\n'
+        'Please copy the full filepath of this Python script from above,\n'
+        'then paste it here using right-click. Then press Enter to begin.\n'
+        )
+        )
 currentFile = currentFile.replace('\\','/')
 scriptdir = currentFile[:currentFile.rfind('/')+1]
 
 binSize = 1 # bin size in mV if rebinning, 0 to not rebin
-### GET FIRST CSV FILES OF EACH EXPERIMENT IN SCRIPT DIRECTORY ###
+# GET FIRST CSV FILES OF EACH EXPERIMENT IN SCRIPT DIRECTORY
 filePathGroups = [scriptdir+f for f in listdir(scriptdir) if '-1.csv' in f]
-### IF ANY VALID CSV FILES FOUND ###
+# IF ANY VALID CSV FILES FOUND
 if len(filePathGroups):
-    ### PROCESS ONE GROUP AT A TIME ###
+    # PROCESS ONE GROUP AT A TIME
     for filePathGroup in filePathGroups:
-        ### GET EXPERIMENT NAME WITHOUT COUNTER OR FILE EXTENSION ###
+        # GET EXPERIMENT NAME WITHOUT COUNTER OR FILE EXTENSION
         expName = filePathGroup.replace('-1.csv','').replace(scriptdir,'')
-        ### COUNT HOW MANY FILES HAVE SAME BASE EXPERIMENT NAME ###
+        # COUNT HOW MANY FILES HAVE SAME BASE EXPERIMENT NAME
         numFiles = 0
-        while path.isfile(filePathGroup.replace('-1.csv','-'+str(numFiles+1)+'.csv')):
+        while path.isfile(filePathGroup.replace(
+                '-1.csv',
+                '-'+str(numFiles+1)+'.csv')):
             numFiles += 1
         if numFiles:
-            ### COUNT THROUGH FILES IN GROUP, ALL OF WHICH ARE REPEAT MEASUREMENTS OF THE SAME EXPERIMENT ###
+            # COUNT THROUGH FILES IN GROUP, ALL OF WHICH ARE REPEAT
+            # MEASUREMENTS OF THE SAME EXPERIMENT
             for i in range(numFiles):
-                ### LOAD CSV TO PANDAS DATAFRAME ###
-                df = pd.read_csv(filePathGroup.replace('-1.csv','-'+str(i+1)+'.csv'),sep=',')
-                ### PROCESS COLUMNS OF PANDAS DATAFRAME ###
+                # LOAD CSV TO PANDAS DATAFRAME
+                df = pd.read_csv(
+                        filePathGroup.replace(
+                            '-1.csv',
+                            '-'+str(i+1) + '.csv'
+                            ),
+                        sep=',',
+                        )
+                # PROCESS COLUMNS OF PANDAS DATAFRAME
                 rawZeta = df['Zeta Potential']
                 rawPower = df['Power']
-                ### DETERMINE NUMBER OF ZETA POTENTIALS ON X AXIS, THIS NUMBER SHOULD BE CONSTANT BETWEEN RUNS ###
+                # DETERMINE NUMBER OF ZETA POTENTIALS ON X AXIS
+                # THIS NUMBER SHOULD BE CONSTANT BETWEEN RUNS
                 nPoints = len(rawZeta)
-                ### IF FIRST FILE ##
+                # IF FIRST FILE
                 if i == 0:
-                    ### CREATE ARRAYS TO STORE MULTIPLE FILES OF DATA IN COLUMNS ###
-                    rawZetas = np.zeros((nPoints,numFiles),dtype=np.float64)
-                    rawPowers = np.zeros((nPoints,numFiles),dtype=np.float64)
-                ### STORE CURRENT FILE DATA IN APPROPRIATE COLUMN ###
+                    # CREATE ARRAYS TO STORE MULTIPLE FILES OF DATA IN COLUMNS
+                    rawZetas = np.zeros((nPoints, numFiles), dtype=np.float64)
+                    rawPowers = np.zeros((nPoints, numFiles), dtype=np.float64)
+                # STORE CURRENT FILE DATA IN APPROPRIATE COLUMN
                 rawZetas[:,i] = rawZeta
                 rawPowers[:,i] = rawPower
-            ### SORT ALL FILES' ZETA POTENTIAL AXES AND PRESERVE PAIRING OF 
-            ### RELATIVE POWER VALUES WITH THEIR CORRESPONDING ZETA POTENTIAL VALUES ###
-            zetasSortedTuple, powerSortedTuple = zip(*sorted(zip(rawZetas.flatten(),rawPowers.flatten())))
+            # SORT ALL FILES' ZETA POTENTIAL AXES AND PRESERVE PAIRING OF
+            # RELATIVE POWER VALUES WITH THEIR CORRESPONDING ZETA POTENTIAL
+            # VALUES
+            zetasSortedTuple, powerSortedTuple = zip(*sorted(zip(
+                rawZetas.flatten(),
+                rawPowers.flatten())))
             if binSize:
-                ### BINNED ARRAY MODE
-                zetas = np.arange(zetasSortedTuple[0],zetasSortedTuple[-1],binSize,dtype=np.float64)
+                # BINNED ARRAY MODE
+                zetas = np.arange(
+                        zetasSortedTuple[0],
+                        zetasSortedTuple[-1],
+                        binSize,
+                        dtype=np.float64,
+                        )
             else:
-                ### ALL UNIQUE ZETA VALUES MODE
-                ### FIND ALL UNIQUE ZETA POTENTIAL VALUES COLLECTED (THEY VARY BETWEEN RUNS) ###
+                # ALL UNIQUE ZETA VALUES MODE
+                # FIND ALL UNIQUE ZETA POTENTIAL VALUES COLLECTED
+                # (THEY VARY BETWEEN RUNS)
                 zetas = np.unique(zetasSortedTuple)
-            ### CALCULATE NUMBER OF BINS OR NUMBER OF UNIQUE ZETA POTENTIAL VALUES IN DATA SET###
+            # CALCULATE NUMBER OF BINS OR NUMBER OF
+            # UNIQUE ZETA POTENTIAL VALUES IN DATA SET
             nBins = len(zetas)
-            powers = np.zeros([nBins],dtype=np.float64)
+            powers = np.zeros([nBins], dtype=np.float64)
             powerStd = deepcopy(powers)
             powerN = deepcopy(powers)
             indsTable = [[]]*nBins
             if binSize:
                 for j,b in enumerate(zetas):
-                    indsTable[j] = np.where(np.all((zetasSortedTuple < b + 0.5*binSize,zetasSortedTuple >= b - 0.5*binSize),axis=0))[0]
+                    indsTable[j] = np.where(np.all((
+                        zetasSortedTuple < b + 0.5*binSize,
+                        zetasSortedTuple >= b - 0.5*binSize,
+                        ),axis=0))[0]
             else:
                 for j,zeta in enumerate(zetas):
                     indsTable[j] = np.where(zetasSortedTuple==zeta)[0]
             for j,inds in enumerate(indsTable):
-                powers[j] = np.mean(np.take(powerSortedTuple,inds)) if len(inds) else 0
-                powerStd[j] = np.std(np.take(powerSortedTuple,inds)) if len(inds) > 1 else 0
+                powers[j] = np.mean(np.take(
+                    powerSortedTuple,inds)) if len(inds) else 0
+                powerStd[j] = np.std(np.take(
+                    powerSortedTuple,inds)) if len(inds) > 1 else 0
                 powerN[j] = len(inds)
-            ### SMOOTHING nSmooths TIMES USING MOVING AVERAGE 1% OF DATA SET IN SIZE ###
+            # SMOOTHING nSmooths TIMES USING MOVING
+            # AVERAGE 1% OF DATA SET IN SIZE
             nSmooths = 2
             powerSmoothed = deepcopy(powers)
-            w = int(nBins*0.01)
+            w = int(nBins * 0.01)
             if w:
                 if binSize:
-                    box = np.ones([w],dtype=np.float64)
+                    box = np.ones(w, dtype=np.float64)
                 else:
-                    box = np.ones([w],dtype=np.float64)
+                    box = np.ones(w, dtype=np.float64)
                 for i in range(nSmooths):
-                    powerSmoothed = np.convolve(powerSmoothed,box,mode='same')/int(nBins*0.01)
+                    powerSmoothed = np.convolve(
+                            powerSmoothed,
+                            box,
+                            mode='same',
+                            ) / int(0.01 * nBins)
             powerSmoothed = powerSmoothed/np.max(powerSmoothed)
-            ### PLOTTING SMOOTHED DATA AND SAVING AS SVG ###
+            # PLOTTING SMOOTHED DATA AND SAVING AS SVG
             if binSize:
                 plt.plot(zetas,powerSmoothed)
             else:
@@ -100,45 +138,124 @@ if len(filePathGroups):
             plt.title(expName)
             plt.savefig(expName+'.svg')
             plt.cla()
-            
-            ### SAVING SMOOTHED AND RAW DATA AS XLSX ###
+           
+            # SAVING SMOOTHED AND RAW DATA AS XLSX
             headers = [expName+str(i) for i in range(1,numFiles+1)]
-            writer = pd.ExcelWriter(scriptdir+expName+'.xlsx', engine='xlsxwriter')
-            df = pd.DataFrame(np.vstack((zetas,powerSmoothed,powerStd,powerN)).T)
-            df.to_excel(writer,sheet_name = 'Summary',header=['Zeta Potential [mV]','Relative Power','Stdev','N'],index = False)
+            writer = pd.ExcelWriter(
+                scriptdir+expName+'.xlsx', engine='xlsxwriter',)
+            df = pd.DataFrame(np.vstack((
+                zetas,
+                powerSmoothed,
+                powerStd,
+                powerN,
+                )).T)
+            df.to_excel(writer,
+                sheet_name='Summary',
+                header=['Zeta Potential [mV]','Relative Power','Stdev','N'],
+                index=False,
+                )
             df = pd.DataFrame([''])
-            df.to_excel(writer,sheet_name = 'RawData',header=['Zeta Potential [mV]'],index = False)
+            df.to_excel(writer,
+                sheet_name='RawData',
+                header=['Zeta Potential [mV]'],
+                index=False,
+                )
             df2 = pd.DataFrame(rawZetas)
-            df2.to_excel(writer,sheet_name = 'RawData',startcol=1,header=headers,index = False)
+            df2.to_excel(writer,
+                sheet_name='RawData',
+                startcol=1,
+                header=headers,
+                index=False,
+                )
             df3 = pd.DataFrame([''])
-            df3.to_excel(writer,sheet_name = 'RawData',startcol=numFiles+1,header=['Relative Power'],index = False)
+            df3.to_excel(writer,
+                sheet_name='RawData',
+                startcol=numFiles+1,
+                header=['Relative Power'],
+                index=False,
+                )
             df4 = pd.DataFrame(rawPowers)
-            df4.to_excel(writer,sheet_name = 'RawData',startcol=numFiles+2,header=headers,index = False)
-            
-            ### PRODUCING SUMMARY LINE PLOT IN EXCEL ###
+            df4.to_excel(writer,
+                sheet_name='RawData',
+                startcol=numFiles+2,
+                header=headers,
+                index=False,
+                )
+           
+            # PRODUCING SUMMARY LINE PLOT IN EXCEL
             fontsize = 20
-            chart = writer.book.add_chart({'type': 'scatter', 'subtype':'straight'})
-            chart.add_series({'name': expName,'categories': ['Summary', 1, 0, nBins, 0], 'values': ['Summary', 1, 1, nBins, 1],'line':       {'color': 'red'}})
-            chart.set_title({'name':expName,'name_font':{'size':20}})
-            chart.set_x_axis({'name': 'Zeta Potential [mV]', 'name_font': {'size': fontsize}, 'num_font': {'size': fontsize},\
-                            'major_gridlines': {'visible': False},'min':-150,'max':150,'major_unit':25,'label_position': 'low'})
-            chart.set_y_axis({'name': 'Relative Power', 'name_font': {'size': fontsize},'num_font': {'size': fontsize},\
-                            'major_gridlines': {'visible': False},'min':0,'label_position': 'low'})
+            chart = writer.book.add_chart({'type': 'scatter',
+                                           'subtype':'straight',})
+            chart.add_series({'name': expName,
+                              'categories': ['Summary', 1, 0, nBins, 0],
+                              'values': ['Summary', 1, 1, nBins, 1],
+                              'line': {'color': 'red'},
+                              })
+            chart.set_title({'name':expName,'name_font':{'size':20},})
+            chart.set_x_axis({'name': 'Zeta Potential [mV]',
+                              'name_font': {'size': fontsize},
+                              'num_font': {'size': fontsize},
+                              'major_gridlines': {'visible': False},
+                              'min': -150,'max': 150, 'major_unit':25,
+                              'label_position': 'low',
+                              })
+            chart.set_y_axis({'name': 'Relative Power',
+                              'name_font': {'size': fontsize},
+                              'num_font': {'size': fontsize},
+                              'major_gridlines': {'visible': False},
+                              'min': 0,'label_position': 'low',
+                              })
             chart.set_legend({'position': 'none'})
             chart.set_size({'width': 1280, 'height': 720})
             writer.sheets['Summary'].insert_chart('F2', chart)
 
-            ### PRODUCING ALL LINES ON ONE PLOT IN EXCEL ###
-            chart = writer.book.add_chart({'type': 'scatter', 'subtype':'straight'})
+            # PRODUCING ALL LINES ON ONE PLOT IN EXCEL
+            chart = writer.book.add_chart({
+                'type': 'scatter',
+                'subtype':'straight',
+                })
             for i in range(numFiles):
-                chart.add_series({'name': expName+'-'+str(i),'categories': ['RawData', 1, i, nBins, i], 'values': ['RawData', 1, numFiles+i+1, nBins, numFiles+i+1],'line':       {'color': 'blue'}})
-            chart.add_series({'name': expName,'categories': ['Summary', 1, 0, nBins, 0], 'values': ['Summary', 1, 1, nBins, 1],'line':       {'color': 'red'}})
-            chart.set_title({'name':expName,'name_font':{'size':20}})
-            chart.set_x_axis({'name': 'Zeta Potential [mV]', 'name_font': {'size': fontsize}, 'num_font': {'size': fontsize},\
-                            'major_gridlines': {'visible': False},'min':-150,'max':150,'major_unit':25,'label_position': 'low'})
-            chart.set_y_axis({'name': 'Relative Power', 'name_font': {'size': fontsize},'num_font': {'size': fontsize},\
-                            'major_gridlines': {'visible': False},'min':0,'label_position': 'low'})
-            chart.set_legend({'position': 'right','name_font': {'size': fontsize}})
-            chart.set_size({'width': 1280, 'height': 720})
+                chart.add_series({
+                    'name': expName+'-'+str(i),
+                    'categories': ['RawData', 1, i, nBins, i],
+                    'values': ['RawData', 1, numFiles+i+1,
+                               nBins, numFiles+i+1],
+                    'line': {'color': 'blue'},
+                    })
+            chart.add_series({
+                'name': expName,
+                'categories': ['Summary', 1, 0, nBins, 0],
+                'values': ['Summary', 1, 1, nBins, 1],
+                'line': {'color': 'red'},
+                })
+            chart.set_title({
+                'name':expName,
+                'name_font':{'size':20},
+                })
+            chart.set_x_axis({
+                'name': 'Zeta Potential [mV]',
+                'name_font': {'size': fontsize},
+                'num_font': {'size': fontsize},
+                'major_gridlines': {'visible': False},
+                'min': -150, 'max': 150,
+                'major_unit': 25,
+                'label_position': 'low',
+                })
+            chart.set_y_axis({
+                'name': 'Relative Power',
+                'name_font': {'size': fontsize},
+                'num_font': {'size': fontsize},
+                'major_gridlines': {'visible': False},
+                'min': 0,
+                'label_position': 'low',
+                })
+            chart.set_legend({
+                'position': 'right',
+                'name_font': {'size': fontsize},
+                })
+            chart.set_size({
+                'width': 1280,
+                'height': 720,
+                })
             writer.sheets['RawData'].insert_chart('D2', chart)
-            writer.save()
+            writer.close()
